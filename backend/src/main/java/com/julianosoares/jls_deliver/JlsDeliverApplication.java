9de759d6f38be9412ab708fdfc1a03c3b357c400
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2021-01-09 23:15:50 -0300

    First commit

---
+++ backend/src/main/java/com/julianosoares/jls_deliver/JlsDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.julianosoares.jls_deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlsDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlsDeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/julianosoares/jls_deliver/JlsDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianosoares.jls_deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlsDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
